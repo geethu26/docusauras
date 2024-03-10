fed293ef044811dc06aa45274bb73a6e4ab81af2
Geethu Joseph <geethujoseph03@gmail.com>
2024-03-10 19:33:25 +0530

    .

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,12 +8,12 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'GJ',
+  tagline: 'Supercalifragilisticexpialedocious',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://geethu26.github.com',
+  url: 'https://geethu26.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/docusaurus/',
