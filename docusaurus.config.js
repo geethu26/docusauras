7fffd1cf4ce05d9e1c177baf0cb0385478256e28
Geethu Joseph <geethujoseph03@gmail.com>
2024-03-10 19:36:28 +0530

    .

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,12 +16,12 @@ const config = {
   url: 'https://geethu26.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docusaurus/',
+  baseUrl: '/docusauras/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'geethu26', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'docusauras', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
